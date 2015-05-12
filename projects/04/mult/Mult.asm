7a56ebdd1d7b1d9646fbd6f393a943caa3dfef61
Amy Hanlon <amyehanlon@gmail.com>
2015-05-12 16:58:21 -0400

    implement multiplication in asm

--- projects/04/mult/Mult.asm
+++ projects/04/mult/Mult.asm
@@ -7,3 +7,24 @@
 // (R0, R1, R2 refer to RAM[0], RAM[1], and RAM[2], respectively.)
 
 // Put your code here.
+
+// Assumptions: R0>=0, R1>=0, and R0*R1<32768
+
+    @R2
+    M=0     // start with R2=0
+(LOOP)
+    @R0
+    D=M
+    @END
+    D;JEQ   // if R0==0, goto END
+    @R1
+    D=M     // D=R1
+    @R2
+    M=M+D   // R2=R2+R1
+    @R0
+    M=M-1   // R0=R0-1
+    @LOOP
+    0;JMP   // goto LOOP
+(END)
+    @END
+    0;JMP
